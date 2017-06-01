ad752daa9e093450b8db7c00354606274c6a7d70
rlatapy <rlatapy@gopro.com>
2017-06-01 15:32:32 +0200

    fix wrong path

--- script_windows64_Install.sh
+++ script_windows64_Install.sh
@@ -4,4 +4,4 @@ cp win32/vlc-2.2.6/sdk/lib/libvlc*.lib /mnt/tera/data/releases/KVLC/vlc2/LibVLC/
 cp -Rf win32/vlc-2.2.6/plugins /mnt/tera/data/releases/KVLC/vlc2/LibVLC/lib/Windows/x64
 
 mkdir -p /mnt/tera/data/releases/KVLC/vlc2/LibVLC/include/vlc/plugins
-cp -Rf win32/vlc-2.2.6/sdk/include /Volumes/data/releases/KVLC/vlc2/LibVLC/include/
+cp -Rf win32/vlc-2.2.6/sdk/include /mnt/tera/data/releases/KVLC/vlc2/LibVLC/
