fee04150f1d92ce0964622947bff12cb0f6b73dd
rlatapy <rlatapy@gopro.com>
2017-06-01 15:27:07 +0200

    Fix install script and add headers

--- script_windows64_Install.sh
+++ script_windows64_Install.sh
@@ -1,3 +1,7 @@
 mkdir -p /mnt/tera/data/releases/KVLC/vlc2/LibVLC/lib/Windows/x64/lib
-cp win32/vlc-2.2/libvlc*.dll /mnt/tera/data/releases/KVLC/vlc2/LibVLC/lib/Windows/x64/lib
-cp -Rf win32/vlc-2.2/plugins /mnt/tera/data/releases/KVLC/vlc2/LibVLC/lib/Windows/x64
+cp win32/vlc-2.2.6/libvlc*.dll /mnt/tera/data/releases/KVLC/vlc2/LibVLC/lib/Windows/x64/lib
+cp win32/vlc-2.2.6/sdk/lib/libvlc*.lib /mnt/tera/data/releases/KVLC/vlc2/LibVLC/lib/Windows/x64/lib
+cp -Rf win32/vlc-2.2.6/plugins /mnt/tera/data/releases/KVLC/vlc2/LibVLC/lib/Windows/x64
+
+mkdir -p /mnt/tera/data/releases/KVLC/vlc2/LibVLC/include/vlc/plugins
+cp -Rf win32/vlc-2.2.6/sdk/include /Volumes/data/releases/KVLC/vlc2/LibVLC/include/
