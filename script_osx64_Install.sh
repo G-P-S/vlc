d103f533eb8982280ccf37d4ec877eb2a72c7832
rlatapy <rlatapy@gopro.com>
2017-06-06 13:58:53 +0200

    Fix - copy directory instead of all files

--- script_osx64_Install.sh
+++ script_osx64_Install.sh
@@ -1,3 +1,3 @@
 mkdir -p /Volumes/data/releases/KVLC/vlc2/LibVLC/lib/Mac/lib
 cp -Rf build/vlc_install_dir/lib/*.dylib /Volumes/data/releases/KVLC/vlc2/LibVLC/lib/Mac/lib
-cp -Rf build/VLC.app/Contents/MacOS/plugins/ /Volumes/data/releases/KVLC/vlc2/LibVLC/lib/Mac
+cp -Rf build/VLC.app/Contents/MacOS/plugins /Volumes/data/releases/KVLC/vlc2/LibVLC/lib/Mac/
