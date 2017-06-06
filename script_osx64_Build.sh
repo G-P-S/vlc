4877d23dce063f344a12ea78d4fe992c8ee4ea7a
rlatapy <rlatapy@gopro.com>
2017-06-06 13:32:36 +0200

    Add path to SDK 11

--- script_osx64_Build.sh
+++ script_osx64_Build.sh
@@ -1,3 +1,3 @@
 mkdir -p build
 cd build
-../extras/package/macosx/build.sh
+../extras/package/macosx/build.sh -k /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.11.sdk/
