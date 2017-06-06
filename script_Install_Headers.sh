6f1f94a0f57dba25eee878aeca275033c49ddf8b
rlatapy <rlatapy@gopro.com>
2017-06-06 14:06:17 +0200

    Update install header script (osx)

--- script_Install_Headers.sh
+++ script_Install_Headers.sh
@@ -1,3 +1,1 @@
-mkdir -p /Volumes/data/releases/KVLC/vlc2/LibVLC/include/vlc/plugins
-cp -Rf include/vlc /Volumes/data/releases/KVLC/vlc2/LibVLC/include/
-cp -f include/*.h /Volumes/data/releases/KVLC/vlc2/LibVLC/include/vlc/plugins
+cp -Rf build/vlc_install_dir/include /Volumes/data/releases/KVLC/vlc2/LibVLC/
