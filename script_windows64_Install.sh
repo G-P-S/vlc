789052f4918bc5d4b8d1b1074491f25d05cbd742
Loic Martin <loic.martin@kolor.com>
2017-10-03 16:15:24 +0200

    fix Windows Install scripts

--- script_windows64_Install.sh
+++ script_windows64_Install.sh
@@ -5,8 +5,8 @@ mkdir -p temp_Windows/plugins
 mkdir -p temp_Windows/include
 cp win32/vlc-3.0.0-git/libvlc*.dll temp_Windows/lib
 cp win32/vlc-3.0.0-git/sdk/lib/libvlc*.lib temp_Windows/lib
-cp win32/vlc-3.0.0-git/plugins/* temp_Windows/plugins
-cp win32/vlc-3.0.0-git/sdk/include/* temp_Windows/include
+cp -R win32/vlc-3.0.0-git/plugins/* temp_Windows/plugins
+cp -R win32/vlc-3.0.0-git/sdk/include/* temp_Windows/include
 
 # apply script to filter plugins
 cd temp_Windows
@@ -16,7 +16,7 @@ cd ..
 # clean Gaia and copy
 rm -Rf /mnt/tera/data/releases/KVLC/master/Windows
 mkdir -p /mnt/tera/data/releases/KVLC/master/Windows
-cp -Rf temp_Windows/* /mnt/tera/data/releases/KVLC/master/Windows
+cp -R temp_Windows/* /mnt/tera/data/releases/KVLC/master/Windows
 
 # destroy temp directory
 rm -Rf temp_Windows
