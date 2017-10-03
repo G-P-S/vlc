aa5aa71a8f604f697b76b6c35a92828a29d1aaa6
Loic Martin <loic.martin@kolor.com>
2017-10-03 15:10:48 +0200

    Modify Install script for Mac & Windows to clean properly. Add python script to clean libraries.

--- script_windows64_Install.sh
+++ script_windows64_Install.sh
@@ -1,7 +1,22 @@
-mkdir -p /mnt/tera/data/releases/KVLC/master/LibVLC/lib/Windows/x64/lib
-cp win32/vlc-3.0.0-git/libvlc*.dll /mnt/tera/data/releases/KVLC/master/LibVLC/lib/Windows/x64/lib
-cp win32/vlc-3.0.0-git/sdk/lib/libvlc*.lib /mnt/tera/data/releases/KVLC/master/LibVLC/lib/Windows/x64/lib
-cp -Rf win32/vlc-3.0.0-git/plugins /mnt/tera/data/releases/KVLC/master/LibVLC/lib/Windows/x64
+# create temp directory, copy libraries&headers
+mkdir -p temp_Windows
+mkdir -p temp_Windows/lib
+mkdir -p temp_Windows/plugins
+mkdir -p temp_Windows/include
+cp win32/vlc-3.0.0-git/libvlc*.dll temp_Windows/lib
+cp win32/vlc-3.0.0-git/sdk/lib/libvlc*.lib temp_Windows/lib
+cp win32/vlc-3.0.0-git/plugins/* temp_Windows/plugins
+cp win32/vlc-3.0.0-git/sdk/include/* temp_Windows/include
 
-mkdir -p /mnt/tera/data/releases/KVLC/master/LibVLC/include/vlc/plugins
-cp -Rf win32/vlc-3.0.0-git/sdk/include /mnt/tera/data/releases/KVLC/master/LibVLC/
+# apply script to filter plugins
+cd temp_Windows
+python ../VLC-make-free.py
+cd ..
+
+# clean Gaia and copy
+rm -Rf /mnt/tera/data/releases/KVLC/master/Windows
+mkdir -p /mnt/tera/data/releases/KVLC/master/Windows
+cp -Rf temp_Windows/* /mnt/tera/data/releases/KVLC/master/Windows
+
+# destroy temp directory
+rm -Rf temp_Windows
